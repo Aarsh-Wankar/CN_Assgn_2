881370abf26c3d2e9bf3b545b85f4ce7493af60f
Abhinav Khot <23110006@iitgn.ac.in>
2025-10-26 12:15:21 +0530

    added the custom dns resolver

---
+++ custom_dns_resolver.py
@@ -0,0 +1,104 @@
+import scapy as sp
+import pandas as pd
+import socket
+from scapy.all import DNS, raw, DNSRR, DNSQR
+import time
+
+class Resolver:
+    def __init__(self, root_server):
+        self.root_server = root_server
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.settimeout(5)
+            
+    def resolve(self, dns_packet, depth=0):
+        if depth > 10:
+            return None
+        if isinstance(dns_packet, bytes):
+            dns_packet = DNS(dns_packet)
+        start_time = time.time()
+        ip_address = self.root_server
+        
+        while True:
+            response = self.query(dns_packet, ip_address)
+
+            if response is None:
+                return None
+
+            if response.ancount:
+                for x in response.an:
+                    if x.type == 1:
+                        print("Resolved IP", x.rdata)
+                        print("time to resolve and ip", time.time() - start_time, x)
+                        return response.an[0].rdata
+                    elif response.ns[0].type == 5: # we get a CNAME RR back
+                        dns_packet = DNS(qd = DNSQR(qname=x.rdata))
+                        ip_address = self.root_server
+
+            elif response.nscount > 0:
+                this_round_done = False
+                
+                if response.ns[0].type == 2:
+                    if response.arcount: #if the server also returns the IP address of the nameserver
+                        ip_addresses = self.get_all_ipv4(response)
+                        if not ip_addresses:
+                            return None
+                        for ip_address in ip_addresses:
+                            next_response = self.query(dns_packet, ip_address)
+                            if next_response:
+                                response = next_response
+                                this_round_done = True
+                                break
+
+                    if this_round_done: #if we already finished this round, continue
+                        continue
+                        
+                    for i in range(response.nscount):
+                        ip_address = self.resolve(DNS(qd = DNSQR(qname=response.ns[i].rdata)), depth + 1) # resolve ip of name server first
+                        if ip_address:
+                            break
+                    else:
+                        return None
+                else:
+                    return None
+            else:
+                return None #nothing to follow :(
+                
+    def get_all_ipv4(self, response):
+        ipv4_addresses = []
+        for i in range(response.arcount):
+            ar = response.ar[i]
+            if ar.type == 1:
+                ipv4_addresses.append(ar.rdata)
+        return ipv4_addresses
+                
+    def query(self, query_dns_packet, ip_address):
+        try:
+            self.socket.sendto(raw(query_dns_packet), (str(ip_address), 53))
+            response_data, _ = self.socket.recvfrom(4096)
+            return DNS(response_data)
+        except:
+            return None
+            
+    def listen(self):
+        listen_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        listen_socket.bind(('127.0.0.1', 5353))
+        while True:
+            dns_packet, client_address = listen_socket.recvfrom(512) # 512 buffer size should be big enough
+            result = self.resolve(dns_packet)
+            if result:
+                response = DNS(dns_packet)
+                response.an = DNSRR(rrname=response.qd.qname, rdata=result)
+                listen_socket.sendto(raw(response), client_address)
+
+if __name__ == "__main__":
+    r = Resolver("198.41.0.4")
+    a = []
+    with open('f_urls_h2.txt') as f:
+        for line in f:
+            url = line.strip()
+            if url:
+                if not url.endswith('.'):
+                    url += '.'
+                a.append(r.resolve(DNS(qd=DNSQR(qname=url))))
+
+    print(len(a), a.count(None))
